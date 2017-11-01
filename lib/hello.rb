5d3d6a88ce7cbf99fb4466886da7746415830faa
Josh Bristol <josh.bristol2@gmail.com>
2017-11-01 19:15:03 +0000

    Hello uses Greeter

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,5 +1,9 @@
+require 'greeter
+'
 # Default is "World"
 # Author: Josh Bristol (josh.bristol2@intracitygeeks.org)
+
 name = ARGV.first || "World"
 
-puts "Hello, #[name}!"
+greeter = Greeter.new(name)
+puts "greeter.greet"
